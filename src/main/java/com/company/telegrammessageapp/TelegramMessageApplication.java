b4843720d6ee1f48870ae14212ea390be464fb45
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-07-10 16:39:51 +0500

    telegram message app project committed

---
+++ src/main/java/com/company/telegrammessageapp/TelegramMessageApplication.java
@@ -0,0 +1,13 @@
+package com.company.telegrammessageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramMessageApplication.class, args);
+    }
+
+}
